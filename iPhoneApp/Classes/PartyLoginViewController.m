4473e2c019ad1f56b6464473ea149bdfe382d825
Matt Graf <graf5@illinois.edu>
2011-10-04 20:33:24 -0500

    Allow for user to return to Party List screen from Party Login screen

--- iPhoneApp/Classes/PartyLoginViewController.m
+++ iPhoneApp/Classes/PartyLoginViewController.m
@@ -14,43 +14,50 @@
 
 @synthesize passwordField, nearbyPartiesButton;
 
-// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 /*
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization.
-    }
-    return self;
-}
-*/
+ // The designated initializer. Override to perform setup that is required before the view is loaded.
+ - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+ self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+ if (self) {
+ // Custom initialization
+ }
+ return self;
+ }
+ */
 
 /*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
+ // Implement loadView to create a view hierarchy programmatically, without using a nib.
+ - (void)loadView {
+ }
+ */
+
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+ - (void)viewDidLoad {
+ [super viewDidLoad];
+ }
+ */
+
+
+/*
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
+	// Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc. that aren't in use.
+	
+	// Release any cached data, images, etc that aren't in use.
 }
 
 - (void)viewDidUnload {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
 }
 
 
@@ -58,22 +65,13 @@
     [super dealloc];
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-	if (textField == passwordField) {
-		[textField resignFirstResponder];
-	}
-	return NO;
-}
 
-//when a button is clicked
 - (IBAction) OnButtonClick:(id) sender {
 	// this view will set our model data
-	//if(sender==nearbyPartiesButton)
-	{
 		//PartyListViewController* partyListViewController = [[PartyListViewController alloc] initWithNibName:@"PartyListViewController" bundle:[NSBundle mainBundle]];
 		//[self.navigationController pushViewController:partyListViewController animated:YES];
 		//[partyListViewController release];
-	}
+	[self.navigationController popViewControllerAnimated:YES];
 }
 
 
