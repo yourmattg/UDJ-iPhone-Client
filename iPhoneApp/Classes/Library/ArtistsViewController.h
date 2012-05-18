e655b125aa7bb9ec49fa9b63c394cb6683002260
Matt Graf <graf5@illinois.edu>
2012-05-18 18:23:25 -0500

    Implemented artist requests and response handling

--- iPhoneApp/Classes/Library/ArtistsViewController.h
+++ iPhoneApp/Classes/Library/ArtistsViewController.h
@@ -1,13 +1,31 @@
-//
-//  ArtistsViewController.h
-//  UDJ
-//
-//  Created by Matthew Graf on 5/18/12.
-//  Copyright (c) 2012 University of Illinois at Urbana-Champaign. All rights reserved.
-//
+/**
+ * Copyright 2011 Matthew M. Graf
+ *
+ * This file is part of UDJ.
+ *
+ * UDJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * UDJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UDJ.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
+#import "RestKit/RestKit.h"
+#import "UDJData.h"
 
 @interface ArtistsViewController : UIViewController
+
+@property(nonatomic,strong) IBOutlet UISearchBar* searchBar;
+@property(nonatomic,strong) IBOutlet UITableView* artistsTableView;
+@property(nonatomic,strong) NSMutableArray* artistsArray;
+@property(nonatomic,strong) IBOutlet UDJData* globalData;
 
 @end
--- iPhoneApp/Classes/Library/ArtistsViewController.m
+++ iPhoneApp/Classes/Library/ArtistsViewController.m
@@ -1,14 +1,28 @@
-//
-//  ArtistsViewController.m
-//  UDJ
-//
-//  Created by Matthew Graf on 5/18/12.
-//  Copyright (c) 2012 University of Illinois at Urbana-Champaign. All rights reserved.
-//
+/**
+ * Copyright 2011 Matthew M. Graf
+ *
+ * This file is part of UDJ.
+ *
+ * UDJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * UDJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UDJ.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "ArtistsViewController.h"
+#import "UDJEventData.h"
 
 @implementation ArtistsViewController
+
+@synthesize artistsTableView, searchBar, artistsArray, globalData;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -33,6 +47,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // get global data
+    globalData = [UDJData sharedUDJData];
+    
+    // intialize artists array
+    artistsArray = [[NSMutableArray alloc] initWithCapacity:50];
 }
 
 - (void)viewDidUnload
@@ -46,6 +66,94 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // show artists name
+    cell.textLabel.text = [artistsArray objectAtIndex: indexPath.row];
+    
+    // cosmetics
+    cell.textLabel.font = [UIFont fontWithName:@"Helvetica" size:16];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
+
+
+#pragma mark - Request methods
+
+-(void)sendArtistsRequest{
+    
+    // gets JSON array of artists
+    RKClient* client = [RKClient sharedClient];
+    
+    // create url /players/player_id/available_music/artists
+    NSString* urlString = client.baseURL;
+    urlString = [urlString stringByAppendingFormat:@"%@%d%@",@"/players/",[UDJEventData sharedEventData].currentEvent.eventId,@"/available_music/artists"];
+    
+    // create request
+    RKRequest* request = [RKRequest requestWithURL:[NSURL URLWithString:urlString] delegate:self];
+    request.queue = client.requestQueue;
+    request.method = RKRequestMethodGET;
+    request.additionalHTTPHeaders = globalData.headers;
+    request.userData = [NSNumber numberWithInt: globalData.requestCount++];
+    
+    //send request
+    [request send]; 
+}
+
+
+#pragma mark - Response handling
+
+-(void)handleArtistResponse:(RKResponse*)response{
+    // clear the current artists array
+    [artistsArray removeAllObjects];
+    
+    // create a JSON parser and parse the artist names
+    RKJSONParserJSONKit* parser = [RKJSONParserJSONKit new];
+    NSArray* responseArray = [parser objectFromString:[response bodyAsString] error:nil];
+    for(int i=0; i<[responseArray count]; i++)
+        [artistsArray addObject: [responseArray objectAtIndex: i]];
+    
+    // reload the table data 
+    [artistsTableView reloadData];
+}
+
+// Handle responses from the server
+- (void)request:(RKRequest*)request didLoadResponse:(RKResponse*)response { 
+    
+    NSLog(@"status code %d", [response statusCode]);
+    
+    //NSNumber* requestNumber = request.userData;
+    NSDictionary* headerDict = [response allHeaderFields];
+    
+    //if(![requestNumber isEqualToNumber: currentRequestNumber]) return;
+    
+    // check if player has ended
+    if(response.statusCode == 404){
+        if([[headerDict objectForKey: @"X-Udj-Missing-Resource"] isEqualToString:@"player"]){}
+            //[self resetToPlayerResultView];
+    }
+    else if ([request isGET] && [response isOK]) {
+        [self handleArtistResponse: response];        
+    }
+    
+    //self.currentRequestNumber = [NSNumber numberWithInt: -1];
 }
 
 @end
