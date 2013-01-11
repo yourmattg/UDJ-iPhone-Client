b19d3afb08a484a7fd320184d590a0f0a748b900
Matt Graf <graf5@illinois.edu>
2013-01-10 19:17:45 -0600

    Implemented UDJRequest intializers

--- iPhoneApp/Classes/Network/UDJRequest.m
+++ iPhoneApp/Classes/Network/UDJRequest.m
@@ -7,6 +7,7 @@
 //
 
 #import "UDJRequest.h"
+#import "UDJClient.h"
 
 @implementation UDJRequest
 
@@ -22,5 +23,39 @@
  request.additionalHTTPHeaders = [UDJData sharedUDJData].headers;
  return request;
  */
+
+#pragma mark - Factory methods and intializers
+
++(UDJRequest*)requestWithMethod:(UDJRequestMethod)method{
+    UDJRequest* request = [[UDJRequest alloc] init];
+    request.method = method;
+    return request;
+}
+
+
++(UDJRequest*)requestWithURL:(NSURL*)url{
+    UDJRequest* request = [[UDJRequest alloc] initWithURL:url];
+    return request;
+}
+
+-(id)initWithURL:(NSURL*)url{
+    if(self = [self init]){
+        self.URL = url;
+    }
+    return self;
+}
+
+#pragma mark - Sending
+
+-(void)send{
+    UDJClient* client = [UDJClient sharedClient];
+    
+    NSURLRequest* request = [NSURLRequest requestWithURL: self.URL];
+    
+}
+
+-(UDJResponse*)sendSynchronously{
+    
+}
 
 @end
